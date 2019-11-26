47df282c75a59ed7d99caffbe97838aae7806a9f
clarkj99 <clark@stontech.net>
2019-11-26 09:54:22 -0500

    Step 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4020 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+
+  fetchPhoto();
+
+  function fetchPhoto() {
+    fetch(imageURL)
+      .then(res => res.json())
+      .then(showPhoto)
+  }
+
+  function showPhoto(photo) {
+    img = document.querySelector('#image');
+    img.src = photo.url;
+
+    h4 = document.querySelector('#name');
+    h4.innerText = photo.name;
+
+    span = document.querySelector('#likes');
+    span.innerText = photo.like_count;
+
+    ul = document.querySelector('#comments');
+    for (const comment of photo.comments) {
+      console.log(comment)
+      li = document.createElement('li');
+      li.innerText = comment.content
+      ul.appendChild(li);
+    }
+  }
 })
